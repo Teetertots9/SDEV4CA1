dae4c21d262b702fcf46d504d51eab7941d32a2f
KevinOToole <kevin.itt.computing@gmail.com>
2019-03-16 11:52:07 +0000

    Added some more departments to the list, cannot for the life of me figure out how to add employees manually without getting a null pointer exception. Will just leave db empty of users for now.

--- CA_1/conf/evolutions/default/3.sql
+++ CA_1/conf/evolutions/default/3.sql
@@ -1,7 +1,14 @@
 # --- !Ups
 
 delete from user;
+delete from department;
+
+insert into department(id,name) values (1,'IT');
+insert into department(id,name) values (2,'Finance');
+insert into department(id,name) values (3,'HR');
+insert into department(id,name) values (4,'Marketing');
+
+
 
 insert into user (type,email,role,name,password) values ( 'm','manager@ofm.com','manager','Mike Manager', 'password');
-
-insert into department(name) values ('IT');
+ 
